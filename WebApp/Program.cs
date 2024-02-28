a58df0154a06953a9ad5d997c1eabb833730f27d
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-02-28 13:39:27 -0700

    not working with loki yet

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using RazorClassLib.Data;
 using RazorClassLib.Services;
+using Serilog;
 using System.Text.Json.Serialization;
 using WebApp.Components;
 using WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -26,6 +31,45 @@ builder.Services.AddSingleton<IOccasionService, OccasionService>();
 builder.Services.AddSingleton<ITicketService, TicketService>();
 builder.Services.AddSingleton<IEnvironmentService, EnvironmentService>();
 builder.Services.AddHealthChecks();
+
+
+#region Serilog
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(hostingContext.Configuration)
+    .WriteTo.OpenTelemetry(options =>
+    {
+        options.Endpoint = $"{Configuration.GetValue<string>("Otlp:Endpoint")}/v1/logs";
+        options.Protocol = Serilog.Sinks.OpenTelemetry.OtlpProtocol.HttpProtobuf;
+        options.ResourceAttributes = new Dictionary<string, object>
+        {
+            ["service.name"] = Configuration.GetValue<string>("Otlp:ServiceName")
+        };
+    }));
+#endregion
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+#region OpenTelemetry
+Action<ResourceBuilder> appResourceBuilder =
+    resource => resource
+        .AddTelemetrySdk()
+        .AddService(Configuration.GetValue<string>("Otlp:ServiceName"));
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(appResourceBuilder)
+    .WithTracing(builder => builder
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddSource("APITracing")
+        //.AddConsoleExporter()
+        .AddOtlpExporter(options => options.Endpoint = new Uri(Configuration.GetValue<string>("Otlp:Endpoint")))
+    )
+    .WithMetrics(builder => builder
+        .AddRuntimeInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter(options => options.Endpoint = new Uri(Configuration.GetValue<string>("Otlp:Endpoint"))));
+#endregion
+
 
 var app = builder.Build();
 
@@ -64,4 +108,4 @@ app.MapRazorComponents<App>()
 
 app.Run();
 
-public partial class Program() { }
+public partial class Program() { public static ConfigurationManager Configuration { get; private set; } }
