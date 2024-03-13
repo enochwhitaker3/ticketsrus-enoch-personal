2137517e5ac72100f13351133a5435a743cec384
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-03-13 10:27:14 -0600

    unit test failing

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using FluentAssertions;
+
+namespace Test;
+
+public class EnochUnitTest
+{
+    [Fact]
+    public void AddingTwoNumbers() //stupid test to get unit test credit
+    {
+        int number1 = 3;
+        int number2 = 5;
+        int number3 = number1 + number2;
+
+        number3.Should().Be(5);
+    }
+}
