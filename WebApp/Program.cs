4065a2c380eefcbdc0b92e5f551761f5f8e6e3a4
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-04-09 10:54:06 -0600

    try

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -54,7 +54,8 @@ public partial class Program()
                         .AddService(serviceName))
                 .AddOtlpExporter(opt =>
                 {
-                    opt.Endpoint = new Uri(builder.Configuration["COLLECTOR_URL"] ?? throw new NullReferenceException("environment variable not set: COLLECTOR_URL"));
+                    // opt.Endpoint = new Uri(builder.Configuration["COLLECTOR_URL"] ?? throw new NullReferenceException("environment variable not set: COLLECTOR_URL"));
+                    opt.Endpoint = new Uri("http://otel-collector:4317/");
                 })
             .AddConsoleExporter();
         });
